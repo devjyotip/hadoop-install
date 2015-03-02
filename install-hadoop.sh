81bbefc3e5053abb5998a11d12509b5a426b1dc9
devjyotip <devjyoti.patra@vizury.com>
2015-03-02 14:10:05 +0530

    changed the permissions for hdfs data dir

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -20,14 +20,12 @@ sudo sh -c 'cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_key
 #ssh localhost
 
 # Download Hadoop and set permissons
-cd ~
-if [ ! -f hadoop-2.2.0.tar.gz ]; then
-	wget http://apache.osuosl.org/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz
-fi
-sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
-cd /usr/local
-sudo mv hadoop-2.2.0 hadoop
-sudo chown -R hduser:hadoop hadoop
+#sudo wget https://archive.apache.org/dist/hadoop/core/hadoop-2.2.0/hadoop-2.2.0.tar.gz
+
+#sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
+#cd /usr/local
+#sudo mv hadoop-2.2.0 hadoop
+#sudo chown -R hduser:hadoop hadoop
 
 # Hadoop variables
 sudo sh -c 'echo export JAVA_HOME=/usr/lib/jvm/jdk/ >> /home/hduser/.bashrc'
@@ -41,40 +39,44 @@ sudo sh -c 'echo export YARN_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
 sudo sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_INSTALL\}/lib/native >> /home/hduser/.bashrc'
 sudo sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\$HADOOP_INSTALL/lib\" >> /home/hduser/.bashrc'
 
+
+# Check that Hadoop is installed
+/usr/local/hadoop/bin/hadoop version
+
+
 # Modify JAVA_HOME 
 cd /usr/local/hadoop/etc/hadoop
 sudo -u hduser sed -i.bak s=\${JAVA_HOME}=/usr/lib/jvm/jdk/=g hadoop-env.sh
 pwd
 
-# Check that Hadoop is installed
-/usr/local/hadoop/bin/hadoop version
 
-# Edit configuration files
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>fs\.default\.name\</name>\<value>hdfs://localhost:9000\</value>\</property>=g' core-site.xml 
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn\.nodemanager\.aux-services</name>\<value>mapreduce_shuffle</value>\</property>\<property>\<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\<value>org\.apache\.hadoop\.mapred\.ShuffleHandler</value>\</property>=g' yarn-site.xml
-  
-sudo -u hduser cp mapred-site.xml.template mapred-site.xml
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>mapreduce\.framework\.name</name>\<value>yarn</value>\</property>=g' mapred-site.xml
+# Get configuration files
+rm -rf core-site.xml
+sudo wget https://raw.githubusercontent.com/devjyotip/hadoop-install/master/core-site.xml
+rm -rf mapred-site.xml
+sudo wget https://github.com/devjyotip/hadoop-install/blob/master/mapred-site.xml
+rm -rf hdfs-site.xml
+sudo wget https://raw.githubusercontent.com/devjyotip/hadoop-install/master/hdfs-site.xml
  
 cd ~
 sudo mkdir -p mydata/hdfs/namenode
 sudo mkdir -p mydata/hdfs/datanode
-
-cd /usr/local/hadoop/etc/hadoop
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>dfs\.replication</name>\<value>1\</value>\</property>\<property>\<name>dfs\.namenode\.name\.dir</name>\<value>file:/home/hduser/mydata/hdfs/namenode</value>\</property>\<property>\<name>dfs\.datanode\.data\.dir</name>\<value>file:/home/hduser/mydata/hdfs/datanode</value>\</property>=g' hdfs-site.xml
+sudo chown -R hduser:hadoop mydata/hdfs
+sudo chmod -R 777 mydata/hdfs
 
 
 # Format Namenode
-#sudo sh -c 'hdfs namenode -format'
+sudo -u hduser /usr/local/hadoop/bin/hdfs namenode -format
 
 # Start Hadoop Service
-#sudo -u hduser start-dfs.sh
-#sudo -u hduser start-yarn.sh
+sudo -u hduser /usr/local/hadoop/sbin/start-all.sh
+#sudo -u hduser /usr/local/hadoop/sbin/start-dfs.sh
+#sudo -u hduser /usr/local/hadoop/sbin/start-yarn.sh
 
 # Check status
-#sudo -u hduser jps
+sudo -u hduser jps
 
 # Example
-# sudo -u hduser cd /usr/local/hadoop
-# sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 2 5
+sudo -u hduser cd /usr/local/hadoop
+sudo -u hduser /usr/local/hadoop/bin/hadoop jar ./usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 2 5
 
