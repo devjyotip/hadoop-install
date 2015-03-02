02366458f9e540270f76d55a10c2ece096fe51d1
devjyotip <devjyoti.patra@vizury.com>
2015-03-02 10:32:20 +0530

    changed hadoop package from 2.6 to 2.2

--- install-hadoop.sh
+++ install-hadoop.sh
@@ -1,51 +1,80 @@
-#!/bin/bash
-MIRROR=http://www.gtlib.gatech.edu/pub/apache/hadoop/common/stable
-VERSION=hadoop-2.6.0
+cd ~ 
+sudo apt-get update
 
-apt-get update
-apt-get -y install openjdk-7-jdk
+# Download java jdk
+sudo apt-get install openjdk-7-jdk
+cd /usr/lib/jvm
+sudo ln -s java-7-openjdk-amd64 jdk
 
-#download hadoop, untar, put in /usr/local
-cd /tmp
-wget "$MIRROR/$VERSION".tar.gz
-tar -xzf "$VERSION".tar.gz
-mv  $VERSION /usr/local
-rm "$VERSION".tar.gz
+# Uncommment to install ssh 
+sudo apt-get install openssh-server
 
-# create user and group
-addgroup hadoop
-useradd  -G hadoop hduser -s /bin/bash -m
+# Add hadoop user
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hduser
+sudo adduser hduser sudo
 
-# app folder
-mkdir -p /app/hadoop/hdfs
-chown -R hduser.hadoop /app/hadoop/hdfs
-chmod 750 /app/hadoop/hdfs
+# Generate keys
+sudo -u hduser ssh-keygen -t rsa -P ''
+sudo sh -c 'cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys'
+#ssh localhost
 
-#modify hadoop-env
-cd /usr/local/$VERSION/conf
-echo "export JAVA_HOME=/usr" >> hadoop-env.sh
-echo "export HADOOP_OPTS=-Djava.net.preferIPv4Stack=true" >> hadoop-env.sh
-
-
-#get configuration files
-rm core-site.xml
-wget https://raw.github.com/okaram/scripts/master/hadoop/conf/core-site.xml
-rm mapred-site.xml
-wget https://raw.github.com/okaram/scripts/master/hadoop/conf/mapred-site.xml
-rm hdfs-site.xml
-wget https://raw.github.com/okaram/scripts/master/hadoop/conf/hdfs-site.xml
-
-# chmod, symbolic links
+# Download Hadoop and set permissons
+cd ~
+if [ ! -f hadoop-2.2.0.tar.gz ]; then
+	wget http://apache.osuosl.org/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz
+fi
+sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
 cd /usr/local
-ln -s $VERSION hadoop
-chown -R hduser.hadoop $VERSION
-chown hduser.hadoop $VERSION
+sudo mv hadoop-2.2.0 hadoop
+sudo chown -R hduser:hadoop hadoop
 
-su - hduser -c "/usr/local/hadoop/bin/hadoop namenode -format"
+# Hadoop variables
+sudo sh -c 'echo export JAVA_HOME=/usr/lib/jvm/jdk/ >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_INSTALL=/usr/local/hadoop >> /home/hduser/.bashrc'
+sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/bin >> /home/hduser/.bashrc'
+sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/sbin >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_MAPRED_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_COMMON_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_HDFS_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export YARN_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_INSTALL\}/lib/native >> /home/hduser/.bashrc'
+sudo sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\$HADOOP_INSTALL/lib\" >> /home/hduser/.bashrc'
 
-#ssh stuff
-su - hduser -c "echo | ssh-keygen -t rsa -P \"\""
-cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
-su - hduser -c "ssh -o StrictHostKeyChecking=no localhost echo "# login once, to add to known hosts
+# Modify JAVA_HOME 
+cd /usr/local/hadoop/etc/hadoop
+sudo -u hduser sed -i.bak s=\${JAVA_HOME}=/usr/lib/jvm/jdk/=g hadoop-env.sh
+pwd
 
-su - hduser -c "/usr/local/hadoop/bin/hadoop namenode -format"
+# Check that Hadoop is installed
+/usr/local/hadoop/bin/hadoop version
+
+# Edit configuration files
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>fs\.default\.name\</name>\<value>hdfs://localhost:9000\</value>\</property>=g' core-site.xml 
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn\.nodemanager\.aux-services</name>\<value>mapreduce_shuffle</value>\</property>\<property>\<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\<value>org\.apache\.hadoop\.mapred\.ShuffleHandler</value>\</property>=g' yarn-site.xml
+  
+sudo -u hduser cp mapred-site.xml.template mapred-site.xml
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>mapreduce\.framework\.name</name>\<value>yarn</value>\</property>=g' mapred-site.xml
+ 
+cd ~
+sudo mkdir -p mydata/hdfs/namenode
+sudo mkdir -p mydata/hdfs/datanode
+
+cd /usr/local/hadoop/etc/hadoop
+sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>dfs\.replication</name>\<value>1\</value>\</property>\<property>\<name>dfs\.namenode\.name\.dir</name>\<value>file:/home/hduser/mydata/hdfs/namenode</value>\</property>\<property>\<name>dfs\.datanode\.data\.dir</name>\<value>file:/home/hduser/mydata/hdfs/datanode</value>\</property>=g' hdfs-site.xml
+
+
+# Format Namenode
+#sudo sh -c 'hdfs namenode -format'
+
+# Start Hadoop Service
+#sudo -u hduser start-dfs.sh
+#sudo -u hduser start-yarn.sh
+
+# Check status
+#sudo -u hduser jps
+
+# Example
+# sudo -u hduser cd /usr/local/hadoop
+# sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 2 5
+
